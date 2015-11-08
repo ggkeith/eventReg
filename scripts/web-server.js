b67a61ce20d6e604076888f0c2fcd3546d692bf7
George Keith <ggkeithv@gmail.com>
2015-11-08 00:51:22 -0600

    added Node

---
+++ scripts/web-server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var rootPath = path.normalize(__dirname + "/../");
+
+app.use(express.static(rootPath + "/app"));
+
+app.listen(8000)
