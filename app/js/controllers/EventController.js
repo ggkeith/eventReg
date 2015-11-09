d7132296bc965a2337b0da2e013a7551c5297f5e
George Keith <ggkeithv@gmail.com>
2015-11-08 21:16:23 -0600

    added CDN for bootstrap and fixed navbar

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,17 @@
+'use strict';
+
+eventsApp.controller("EventController",
+  function EventController($scope) {
+    $scope.event = {
+      name: "Angular Boot Camp",
+      date: "1/1/2016",
+      time: "10:30 am",
+      location: {
+        address: "3820 American Drive, Suite 110",
+        city: "Plano",
+        state: "TX"
+      },
+      imageUrl: "img/angularjs-logo.png"
+    }
+  }
+);
